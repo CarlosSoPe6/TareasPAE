cbb0f5fdf762ffc4595e33848f7f1218105fe196
CarlosSoPe6 <carlos348@outlook.com>
2020-03-24 13:44:49 -0600

    2 files added

---
+++ src/app/s10-productos/Producto.ts
@@ -0,0 +1,18 @@
+export class Product {
+  uid: number;
+  nombre: string;
+  marca: string;
+  descripcion: string;
+  precio: number;
+  existencia: number;
+  imagen: string;
+  constructor(uid, nombre, marca, descripcion, precio, existencia) {
+    this.uid = uid;
+    this.nombre = nombre;
+    this.marca = marca;
+    this.descripcion = descripcion;
+    this.precio = precio;
+    this.existencia = existencia;
+    this.imagen = `https://picsum.photos/${uid}/1/100/100`;
+  }
+}
