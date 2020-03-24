af35ce984107e5682fe6240f30380121ec6a9b93
CarlosSoPe6 <carlos348@outlook.com>
2020-03-24 13:38:49 -0600

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { S10ProductosComponent } from './s10-productos/s10-productos.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, S10ProductosComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/s10-productos/s10-productos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-s10-productos',
+  templateUrl: './s10-productos.component.html',
+  styleUrls: ['./s10-productos.component.css']
+})
+export class S10ProductosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
