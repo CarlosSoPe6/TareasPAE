0e5ab2e4f73b6f5b7263df6d7742f3b83705d0e5
Carlos Soto Pérez <carlos348@outlook.com>
2020-03-24 17:19:06 -0600

    Código desde computador. No verificado

--- src/app/s10-productos/Producto.ts
+++ src/app/s10-productos/Producto.ts
@@ -1,4 +1,4 @@
-export class Product {
+export class Producto {
   uid: number;
   nombre: string;
   marca: string;
--- src/app/s10-productos/s10-productos.component.ts
+++ src/app/s10-productos/s10-productos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Producto} from './Producto';
 
 @Component({
   selector: 'app-s10-productos',
@@ -7,9 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class S10ProductosComponent implements OnInit {
 
-  constructor() { }
+  productos: Producto[];
+  busquedaProductos: Producto[];
+  busqueda: string;
+  existencia: boolean;
+  ordenar: boolean;
+  resaltar: boolean;
+
+  constructor() {
+    this.busquedaProductos = [];
+    this.productos = [
+
+    ];
+    this.busqueda = "";
+    this.existencia = false;
+    this.ordenar = false;
+    this.resaltar = false;
+  }
+
+  onExistencia() {
+    this.existencia = !this.existencia;
+    this.onBusqueda();
+  }
+
+  onOrdenar() {
+    this.ordenar = !this.ordenar;
+    this.onBusqueda();
+  }
+
+  onResaltar() {
+    this.resaltar = !this.resaltar;
+  }
+
+  onBusqueda() {
+    let busquedaUpper = this.busqueda.toUpperCase();
+    this.busquedaProductos = this.productos.filter((prod: Producto) => {
+      if(prod.nombre.toUpperCase().includes(busquedaUpper) || prod.descripcion.toUpperCase().includes(busquedaUpper)){
+        return prod;
+      }
+    });
+    
+    if(this.existencia){
+      this.busquedaProductos = this.productos.filter((prod: Producto) => {
+        if(prod.existencia > 0){
+          return prod;
+        }
+      });
+    }
+
+    if(this.ordenar){
+      this.busquedaProductos = this.busquedaProductos.sort((p1, p2) => p2.precio - p1.precio);
+    }
+  }
 
   ngOnInit() {
   }
+
 
 }
